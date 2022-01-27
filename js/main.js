9359108c45003bce020bb603f10bcbd397395f71
SanyaMad <90633231+SanyaMad@users.noreply.github.com>
2022-01-27 10:12:19 +0300

    update 27.01

--- js/main.js
+++ js/main.js
@@ -1,3 +1,50 @@
+$(document).ready(function () {
+    var modal = $('.modal'),
+        modalBtn = $('[data-toggle="modal"]'),
+        closeBtn = $('.modal__close');
+    
+    modalBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+    closeBtn.on('click', function() {
+        modal.toggleClass('modal--visible');
+    })
+
+$(document).on('keydown', function(e) {
+    if (e.keyCode == 27)
+    modal.removeClass('modal--visible')
+});
+
+});
+
+
+
+  
+
+
+
+
+
+
+
+
+
+/*
+document.addEventListener('click', (e) => {
+    if (e.target.classList === modal.classList) {
+        modal.classList.toggle('modal--visible');
+    };
+});
+
+document.addEventListener('keydown', (e) => {
+    if(e.key == 'Escape') {
+        if (modal.classList.contains('modal--visible')) {
+            modal.classList.toggle('modal--visible');
+        };
+    }}); */
+
+
+/*
 document.addEventListener("DOMContentLoaded", function(event) {
     const modal = document.querySelector('.modal');
     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
@@ -12,3 +59,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     CloseBtn.addEventListener('click', switchModal)
 });
+*/
+
